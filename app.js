4e7ea58d3cd9852a063bc20e3abfcd22f89b4622
linhhm24 <linh.hm24@gmail.com>
2020-02-29 14:20:02 +0700

    add file

---
+++ app.js
@@ -0,0 +1,8 @@
+let array1 = prompt('Nhap vao chuoi 1');
+let array2 = prompt('Nhap vao chuoi 2');
+
+if(array1===array2){
+    alert('Hai chuoi giong nhau')
+}else {
+    alert('Hai chuoi khong giong nhau');
+}
